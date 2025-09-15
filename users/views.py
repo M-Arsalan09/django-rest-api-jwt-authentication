ceb121c1a5d40c44067e1c9d696020946133d6ce
M_Arsalan <arsalankamran80@gmail.com>
2025-09-16 00:40:00 +0500

    Feat: Added register, get token and refresh token endpoints

--- users/views.py
+++ users/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .serializers import RegisterSerializer
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        return Response(
+            {
+                "details":{
+                    "message": "User registered successfully",
+                    "data": {
+                    "id": user.id,
+                    "username": user.username,
+                    "email": user.email
+                },
+                } 
+            },
+            status=status.HTTP_201_CREATED
+        )
